ab6d05fc8bb85b3809774f5127b7442b853731e7
Eddy Alvaro <e.alvaro.arenas@gmail.com>
2025-10-03 16:15:24 -0500

    feat: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <p className="text-center">15% OFF EN TODO CON EL CÓDIGO: XXXOFF</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
